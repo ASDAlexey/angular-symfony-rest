dc5e94dc57f94c123a6b8f8c3a39c8a9f758a7b5
Alexey Popov <alexey.popov@umbrella-web.com>
2017-11-30 16:51:11 +0300

    updated api url

--- src/app/shared/shared.constant.ts
+++ src/app/shared/shared.constant.ts
@@ -5,7 +5,8 @@ export class SharedConstants {
   public static DOMAIN_URL: string = document.location.protocol + '//' + document.location.host;
   private static isLocalhost: boolean = !!SharedConstants.DOMAIN_URL.match(/localhost:4200/);
   public static ASSETS_URL: string = `${SharedConstants.DOMAIN_URL + '/assets'}`;
-  public static API_URL: string = SharedConstants.isLocalhost ? `https://api.symfony-rest.local` : SharedConstants.DOMAIN_URL.replace('app', 'api');
+  public static API_URL: string = SharedConstants.isLocalhost ?
+    `https://api.symfony-rest.local` :
+    SharedConstants.DOMAIN_URL.replace('https://', 'https://api.');
   public static STUB_URL: string = `${SharedConstants.ASSETS_URL}/stub`;
-  public static LANGS: [string] = ['en', 'sv']
 }
