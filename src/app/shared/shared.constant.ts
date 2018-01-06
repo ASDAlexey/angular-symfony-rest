b68b48f17ccd9ebd1728457e8811f9e1720d766b
Alexey Popov <alexey.popov@umbrella-web.com>
2018-01-06 20:59:10 +0300

    updated API dynamic api url
    (cherry picked from commit e639b04)

--- src/app/shared/shared.constant.ts
+++ src/app/shared/shared.constant.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SharedConstants {
-  public static DOMAIN_URL: string = document.location.protocol + '//' + document.location.host;
+  static PROTOCOL: string = document.location.protocol;
+  static HOST: string = document.location.host;
+  static DOMAIN_URL: string = SharedConstants.PROTOCOL + '//' + SharedConstants.HOST;
   private static isLocalhost: boolean = !!SharedConstants.DOMAIN_URL.match(/localhost:4200/);
-  public static ASSETS_URL: string = `${SharedConstants.DOMAIN_URL + '/assets'}`;
-  public static API_URL: string = SharedConstants.isLocalhost ?
+  static ASSETS_URL: string = `${SharedConstants.DOMAIN_URL + '/assets'}`;
+  static API_URL: string = SharedConstants.isLocalhost ?
     `https://api.symfony-rest.local` :
-    SharedConstants.DOMAIN_URL.replace('https://', 'https://api.');
-  public static STUB_URL: string = `${SharedConstants.ASSETS_URL}/stub`;
+    SharedConstants.DOMAIN_URL.replace(
+      `${SharedConstants.PROTOCOL}://`,
+      `${SharedConstants.PROTOCOL}://api.`,
+    );
+  static STUB_URL: string = `${SharedConstants.ASSETS_URL}/stub`;
 }
